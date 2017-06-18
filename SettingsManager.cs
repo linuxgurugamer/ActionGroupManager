42868a7b2ef39c1a4a21d054e76b661e1cc332c3
Alshain01 <alshain01@suddenlink.net>
2017-06-17 20:34:20 -0500

    Change License to MIT

--- SettingsManager.cs
+++ SettingsManager.cs
@@ -1,10 +1,4 @@
-﻿//Copyright © 2013 Dagorn Julien (julien.dagorn@gmail.com)
-//This work is free. You can redistribute it and/or modify it under the
-//terms of the Do What The Fuck You Want To Public License, Version 2,
-//as published by Sam Hocevar. See the COPYING file for more details.
-
-
-using KSP.IO;
+﻿using KSP.IO;
 
 namespace ActionGroupManager
 {
