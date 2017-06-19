1678f257ad0b08423cb43adc638e0e6f6f230e12
Alshain01 <alshain01@suddenlink.net>
2017-06-19 13:47:34 -0500

    Change style

--- Style.cs
+++ Style.cs
@@ -16,7 +16,7 @@ namespace ActionGroupManager
 
         public static GUIStyle LabelExpandStyle;
 
-        static bool UseKSPSkin = false;
+        static bool UseKSPSkin = true;
 
         static Style()
         {
